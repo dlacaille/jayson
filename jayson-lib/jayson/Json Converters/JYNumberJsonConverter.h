1ddc948887b00a823016ad46cbd7ce77cc206e0e
Hugo Crochetière <h.crochetiere@appcom.ca>
2015-05-06 18:00:48 -0400

    - Initialize the JYNumberJsonConverter

---
+++ jayson-lib/jayson/Json Converters/JYNumberJsonConverter.h
@@ -0,0 +1,14 @@
+//
+//  JYNumberJsonConverter.h
+//  jayson-lib
+//
+//  Created by Hugo Crochetière on 2015-05-06.
+//  Copyright (c) 2015 ldom66. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "JYJsonConverter.h"
+
+@interface JYNumberJsonConverter : NSObject<JYJsonConverter>
+
+@end
---
+++ jayson-lib/jayson/Json Converters/JYNumberJsonConverter.m
@@ -0,0 +1,27 @@
+//
+//  JYNumberJsonConverter.m
+//  jayson-lib
+//
+//  Created by Hugo Crochetière on 2015-05-06.
+//  Copyright (c) 2015 ldom66. All rights reserved.
+//
+
+#import "JYNumberJsonConverter.h"
+
+@implementation JYNumberJsonConverter
+
+- (NSString *)toString:(id)obj {
+    return ((NSNumber *)obj).stringValue;
+}
+
+- (id)fromString:(NSString *)string {
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+    formatter.numberStyle = NSNumberFormatterDecimalStyle;
+    return [formatter numberFromString:string];
+}
+
+- (BOOL)canConvert:(Class)objectClass {
+    return [objectClass isSubclassOfClass:[NSNumber class]];
+}
+
+@end
