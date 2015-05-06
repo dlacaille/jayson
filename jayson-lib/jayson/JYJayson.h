66f471006815c7d7677b8c9329ee26a0c9013197
Dominic Lacaille <d.lacaille@appcom.ca>
2015-05-06 15:12:58 -0400

    - Created method prototypes.

---
+++ jayson-lib/jayson/JYJayson.h
@@ -0,0 +1,28 @@
+//
+//  JYJayson.h
+//  JYJayson
+//
+//  Created by Dominic Lacaille on 2015-05-06.
+//  Copyright (c) 2015 ldom66. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JYJayson : NSObject
+
+/**
+ * Serializes an object to JSON.
+ * @return A serialized string representing the object.
+ */
+- (NSString *)serializeObject:(id)obj;
+
+/**
+ * Deserializes a JSON string into an object.
+ *
+ * @param json JSON string to deserialize.
+ * @param objectClass Class of the object to return.
+ * @return The deserialized object of "objectClass" type.
+ */
+- (id)deserializeObject:(NSString *)json withClass:(Class *)objectClass;
+
+@end
---
+++ jayson-lib/jayson/JYJayson.m
@@ -0,0 +1,15 @@
+//
+//  JYJayson.h
+//  JYJayson
+//
+//  Created by Dominic Lacaille on 2015-05-06.
+//  Copyright (c) 2015 ldom66. All rights reserved.
+//
+
+#import "JYJayson.h"
+
+@implementation JYJayson
+
+
+
+@end
