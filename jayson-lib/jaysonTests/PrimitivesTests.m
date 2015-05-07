e942dbbecad6f585cfcd39cb8893ccae988f9f00
Hugo Crochetière <h.crochetiere@appcom.ca>
2015-05-07 16:11:40 -0400

    - Add basic test for nsdata

--- jayson-lib/jaysonTests/PrimitivesTests.m
+++ jayson-lib/jaysonTests/PrimitivesTests.m
@@ -43,14 +43,13 @@
     XCTAssertEqualObjects((@[@"test",@"test2",@"test3"]), [JYJayson deserializeObject:@"[\"test\",\"test2\",\"test3\"]" withClass:[NSArray class]]);
     XCTAssertEqualObjects((@[@1,@2,@3,@4,@5]), [JYJayson deserializeObject:@"[1,2,3,4,5]" withClass:[NSArray class]]);
     
-    /* TEST DATA */
+    /* NSDATA */
 
-    NSData *data = UIImageJPEGRepresentation([UIImage imageNamed:@"github.jpeg"],1);
+    NSData *data = [NSKeyedArchiver archivedDataWithRootObject:[UIColor greenColor]];
     NSString *encoded = [data base64EncodedString];
+    
     XCTAssertEqualObjects(encoded, [JYJayson serializeObject:data]);
     
-    data = UIImageJPEGRepresentation([UIImage imageNamed:@"github.jpeg"],1);
-    encoded = [data base64EncodedString];
     XCTAssertEqualObjects(data, [JYJayson deserializeObject:encoded withClass:[NSData class]]);
     
 }
