774ba6291afed9270c8466a4a17acaabe6aeab1c
Dominic Lacaille <d.lacaille@appcom.ca>
2015-05-07 14:55:35 -0400

    - Added a test.

--- jayson-lib/jaysonTests/PrimitivesTests.m
+++ jayson-lib/jaysonTests/PrimitivesTests.m
@@ -16,7 +16,6 @@
 
 @implementation PrimitivesTests
 
-
 - (void)testExample {
     /* NSSTRING */
     XCTAssertEqualObjects(@"\"test\"", [JYJayson serializeObject:@"test"]);
@@ -32,6 +31,7 @@
     XCTAssertEqualObjects(@"0.45", [JYJayson serializeObject:[NSNumber numberWithFloat:0.45f]]);
     XCTAssertEqualObjects(@"1", [JYJayson serializeObject:[NSNumber numberWithBool:true]]);
     XCTAssertEqualObjects(@"12", [JYJayson serializeObject:[NSNumber numberWithInt:12]]);
+    XCTAssertEqualObjects(@"0", [JYJayson serializeObject:[NSNumber numberWithInt:0x0]]);
     XCTAssertEqualObjects(@"500", [JYJayson serializeObject:[NSNumber numberWithLong:500]]);
     XCTAssertEqualObjects(@"1", [JYJayson serializeObject:@(1)]);
     XCTAssertEqualObjects(@(12), [JYJayson deserializeObject:@"12" withClass:[NSNumber class]]);    
